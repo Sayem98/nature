9757ac99fdd538c69d0002b0d7b19c8d0b534e5a
Sayem98 <sayemabedin.bd@gmail.com>
2024-05-27 02:59:37 +0600

    mongooes

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
+const dotenv = require('dotenv');
 const app = require('./app');
-const port = 3000;
+const mongoose = require('mongoose');
+
+dotenv.config({
+  path: './config.env',
+});
+
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+const local_db = process.env.DATABASE_LOCAL;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful');
+  });
+console.log(process.env.NODE_ENV);
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port} `);
 });

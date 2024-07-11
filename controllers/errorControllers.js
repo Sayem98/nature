26bb53e5606bda216f91771493f2b37f4d3b9a91
Sayem98 <sayemabedin.bd@gmail.com>
2024-07-12 02:18:03 +0600

    converting mongoose error to our custom app error

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const sendErrForDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -25,6 +26,11 @@ const sendErrProduction = (err, res) => {
   }
 };
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Error';
@@ -32,6 +38,8 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrForDev(err, res);
   } else {
-    sendErrProduction(err, res);
+    let error = { ...err };
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrProduction(error, res);
   }
 };

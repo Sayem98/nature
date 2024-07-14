6f48539deb94fec149a6fd861e38fa75eeaf42b3
Sayem98 <sayemabedin.bd@gmail.com>
2024-07-14 23:56:57 +0600

    error handling

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Middlewere');
+  console.log(
+    process.env.NODE_ENV === 'development'
+      ? '---Development---'
+      : '---Production---',
+  );
   next();
 });
 
--- server.js
+++ server.js
@@ -11,15 +11,15 @@ const db = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 // const local_db = process.env.DATABASE_LOCAL;
-mongoose
-  .connect(db, {
-
-  })
-  .then((con) => {
-    console.log('DB connection successful');
-  });
+mongoose.connect(db, {}).then((con) => {
+  console.log('DB connection successful');
+});
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port} `);
 });
+
+// process.on('unhandledRejection', (err) => {
+//   console.log(err.name, err.message);
+// });
